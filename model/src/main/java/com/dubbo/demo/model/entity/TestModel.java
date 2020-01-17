500aead0d17b0a3c6961ed2c87834122ee122f8f
郭亚龙 <guoyalong@zto.com>
2020-01-17 09:56:13 +0800

    实体类

---
+++ model/src/main/java/com/dubbo/demo/model/entity/TestModel.java
@@ -0,0 +1,10 @@
+package com.dubbo.demo.model.entity;
+
+import java.io.Serializable;
+
+/**
+ * @author guoyalong
+ * @date2020/1/17 9:52
+ */
+public class TestModel implements Serializable {
+}
---
+++ model/src/main/java/com/dubbo/demo/model/mapper/TestMapper.java
@@ -0,0 +1,4 @@
+package com.dubbo.demo.model.mapper;
+
+public interface TestMapper {
+}
