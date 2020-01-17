1d32559a57109397f7e1779ef45a444d843923f0
郭亚龙 <guoyalong@zto.com>
2020-01-17 14:20:20 +0800

    测试测试

--- model/src/main/java/com/dubbo/demo/model/entity/TestModel.java
+++ model/src/main/java/com/dubbo/demo/model/entity/TestModel.java
@@ -9,6 +9,16 @@ import java.io.Serializable;
 public class TestModel implements Serializable {
     private String name;
 
+    private Integer age;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
     public String getName() {
         return name;
     }
